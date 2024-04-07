a8666610013ac27b9223785c66db5edbf718f2df
electr0n4ik <andreyshka3@gmail.com>
2024-04-07 10:37:55 +0300

    Create models.

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 <h1 align="center">Banner service</h1>
 
 <p align="center">
-  empty
+  Решенные возникшие вопросы.
+</p>
+<p>
+  1. 
 </p>
--- main/models.py
+++ main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Feature(models.Model):
+    feature_id =  models.IntegerField(unique=True)
+
+class Banner(models.Model):
+    tag_ids = models.ManyToManyField('Tag', related_name='tags')
+    feature_id = models.ForeignKey(Feature, on_delete=models.CASCADE)
+    content = models.JSONField()  # '{"title": "some_title", "text": "some_text", "url": "some_url"}'
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['feature_id'], name='unique_banner_tag')
+        ]
+
+
+class Tag(models.Model):
+    tag_id = models.IntegerField(unique=True)
