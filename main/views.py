dbb7af42d3aa498cc33c23fb20f34df20ca757ab
electr0n4ik <andreyshka3@gmail.com>
2024-04-07 13:14:57 +0300

    Correct response by filters tag_id, feature_id.

--- main/views.py
+++ main/views.py
@@ -10,16 +10,45 @@ def user_banner_view(request):
         tag_id = request.GET.get("tag_id", None)
         feature_id = request.GET.get("feature_id", None)
 
-        banner = models.BannerTag.objects.filter(
-            tag_id=tag_id,
-            feature_id=feature_id
-        ).select_related("banner", "tag", "feature")
+        # banners = models.Banner.objects.filter(feature=feature_id)
+        # banner_tag = models.BannerTag.objects.filter(
+        #     tag_id=tag_id,
+        #     banner__feature_id=feature_id
+        # ).select_related("banner", "tag").all()
+        # Получение всех баннеров, связанных с конкретным тегом и функцией через модель BannerTag
+        # banners = models.Banner.objects.filter(
+        #     bannertag__tag_id=tag_id,
+        #     feature_id=feature_id,
+        #     bannertag__isnull=False  # Условие для проверки наличия связанных объектов BannerTag
+        # )
+        # Получение всех баннеров, связанных с конкретной функцией (feature)
+        banners_by_feature = models.Banner.objects.filter(feature_id=feature_id)
+        print("Banners by feature IDs:", list(banners_by_feature.values_list('id', flat=True)))
 
+        # Получение всех объектов BannerTag, связанных с указанным тегом
+        banner_tags_by_tag = models.BannerTag.objects.filter(tag_id=tag_id)
+        print("Banner tags by tag IDs:", list(banner_tags_by_tag.values_list('id', flat=True)))
+
+        # Находим общие баннеры между двумя наборами
+        # banners = banner_tags_by_tag.filter(banner__in=banners_by_feature)
+        # print("Common banner tags IDs:", list(banners.values_list('id', flat=True)))
+        banners = models.Banner.objects.filter(bannertag__in=banner_tags_by_tag)
+        print("Common banners IDs:", list(banners.values_list('id', flat=True)))
+
+        for i in banners:
+            print(i)
+            banner = i
+        if banner:
+            return JsonResponse({
+                "title": banner.title, 
+                "text": banner.description, 
+                "url": banner.url
+                }, status=200)
         return JsonResponse({
-            "title": banner.title, 
-            "text": banner.description, 
-            "url": banner.url
-            }, status=200)
+                "title": None, 
+                "text": None, 
+                "url": None
+                }, status=200)
     else:
         return JsonResponse({
             'error': 'Method not allowed'
