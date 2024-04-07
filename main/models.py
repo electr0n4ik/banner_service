33536da86d08b6703743337653647404bb24ead0
electr0n4ik <andreyshka3@gmail.com>
2024-04-07 10:44:51 +0300

    Correct models.

--- main/models.py
+++ main/models.py
@@ -1,20 +1,16 @@
 from django.db import models
 
-
 class Feature(models.Model):
-    feature_id =  models.IntegerField(unique=True)
+    feature_id = models.IntegerField(unique=True)
 
 class Banner(models.Model):
-    tag_ids = models.ManyToManyField('Tag', related_name='tags')
-    feature_id = models.ForeignKey(Feature, on_delete=models.CASCADE)
+    feature = models.ForeignKey(Feature, on_delete=models.CASCADE)
     content = models.JSONField()  # '{"title": "some_title", "text": "some_text", "url": "some_url"}'
     is_active = models.BooleanField(default=True)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['feature_id'], name='unique_banner_tag')
-        ]
-
-
 class Tag(models.Model):
     tag_id = models.IntegerField(unique=True)
+
+class BannerTag(models.Model):
+    banner = models.ForeignKey(Banner, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
